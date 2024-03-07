c4bc90ed17a634a469eca112ed0a68c136c3e3a9
Sabrina Marques <sabrinamarques0240@gmail.com>
2024-03-06 22:29:07 -0300

    Cadastro de login

---
+++ projeto-login/script/pasta.js
@@ -0,0 +1,15 @@
+function login(){
+    const email = document.getElementById("email");
+    const senha = document.getElementById("senha");
+    console.log(email, senha)
+    
+    const cadastro = JSON.parse(localStorage.getItem("cadastro"))
+    console.log(cadastro) 
+    if(cadastro === null){
+        document.getElementById("mensagem").innerText = `Cadastre-se primeiro`
+    }else if(email.value === cadastro.email && senha.value === cadastro.senha){
+        window.location.href = "./entry.html"
+    }else{
+        document.getElementById("mensagem").innerText = `Senha ou e-mail invalidos`
+    }
+}
--- projeto-login/script/script.js
+++ projeto-login/script/script.js
@@ -1,6 +1,27 @@
-const naoMostrar = document.getElementsByTagName("div")[0]
+const name = document.getElementById("name")
+const especie = document.getElementById("especie")
+const tamanhoAnimal = document.getElementById("tamanhoAnimal")
+const idadeAnimal = document.getElementById("idadeAnimal")
+const nomeDono = document.getElementById("nomeDono")
+const email = document.getElementById("email")
+const contato = document.getElementById("contato")
+const senha = document.getElementById("senha")
 
-function esconder(){
-    naoMostrar.classList.add('hide')
-    naoMostrar.classList.remove("hide")
+class Cadastros{
+    constructor(name, especie, tamanhoAnimal, idadeAnimal, nomeDono, email, contato, senha){
+        this.name = name
+        this.especie = especie
+        this.tamanhoAnimal = tamanhoAnimal
+        this.idadeAnimal = idadeAnimal
+        this.nomeDono = nomeDono
+        this.email = email
+        this.contato = contato
+        this.senha = senha
+    }
+}
+
+function redirecionar(){
+    const dados = new Cadastros(name.value, especie.value, tamanhoAnimal.value, idadeAnimal.value, nomeDono.value, email.value, contato.value, senha.value)
+    localStorage.setItem("cadastro", JSON.stringify(dados))
+    window.location.href = "./entry.html"
 }
